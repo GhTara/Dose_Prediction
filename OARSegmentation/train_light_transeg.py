41919da3d08f79e4d0912b40c59bdf48abe1058b
Tara <gheshlaghitara@gmail.com>
2023-07-16 12:15:03 +0330

    docs: change the documentation

--- OARSegmentation/train_light_transeg.py
+++ OARSegmentation/train_light_transeg.py
@@ -13,7 +13,6 @@ import torch
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-# from pytorch_lightning.callbacks import TQDMProgressBar, ProgressBarBase
 from pytorch_lightning.loggers import MLFlowLogger
 
 from OARSegmentation.DataLoader.provided_dataset import get_dataset
